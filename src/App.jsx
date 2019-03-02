e143c50cf460a8976bc6751a29467a637e34f399
Joe V <joevillegasisawesome@gmail.com>
2019-03-01 19:16:00 -0800

    Basic modification of blanks with random generators

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import './App.css';
 //import AuthButton from './components/AuthButton';
 import AuthIDInput from './components/AuthIDInput';
 import MadLibHandler from './components/MadLibHandler'
+import CanvasDataHandler from './components/CanvasDataHandler'
 
 import credentials from './auth/credentials.json'
 
@@ -19,12 +20,68 @@ class App extends Component {
     console.log('sadfas')
     if (!(credentials.canvas.access_token === "")) {
       var generatedMadLib = await this.MadLibHandler.fetchStory()
-      generatedMadLib = await this.MadLibHandler.modifyBlanks(generatedMadLib, ['lel'])
+
+      var canvasBlankFillers = await this.matchBlanksFromCanvas(generatedMadLib.blanks)
+
+      generatedMadLib = await this.MadLibHandler.modifyBlanks(generatedMadLib, canvasBlankFillers)
 
       ReactDOM.render(<Story story={generatedMadLib} />,
         document.getElementById('root'))
     }
   }
+
+  matchBlanksFromCanvas = (blanks) => {
+    let newBlanks = blanks
+
+
+    for (var i = 0; i < blanks.length; i++) {
+      if (blanks[i] === 'a place' || blanks[i] === 'foreign country') {
+        blanks[i] = this.CanvasDataHandler.fetchRandomCourse()
+      }
+      if (blanks[i] === 'noun') {
+        blanks[i] = this.CanvasDataHandler.fetchRandomCourse()
+      }
+
+      //TODO: All other cases
+    }
+
+    return newBlanks
+
+  }
+
+  fetchRandomNoun() {
+    var dice = Math.random() * 12;
+
+    if (dice < 2) {
+      return this.CanvasDataHandler.fetchRandomCourse
+    } else if (dice < 4) {
+      return this.CanvasDataHandler.fetchRandomAssignment
+    } else if (dice < 6) {
+      return this.CanvasDataHandler.fetchRandomQuiz
+    } else if (dice < 8) {
+      return this.CanvasDataHandler.fetchRandomGroup
+    } else if (dice < 10) {
+      return this.CanvasDataHandler.fetchRandomStudent
+    } else if (dice < 12) {
+      return this.CanvasDataHandler.fetchRandomAdmin
+
+    }
+  }
+
+  fetchRandomAbjective() {
+    //TODO: How are you feeling in your classes?
+
+    var dice = Math.random() * 12;
+  }
+
+  fetchRandomLiquid() {
+
+  }
+
+  fetchRandomAnimal() {
+    //TODO: What is your school mascot?
+  }
+
 
   render() {
 
@@ -48,6 +105,7 @@ class App extends Component {
           </p>
         </footer>
         <MadLibHandler onRef={ref => (this.MadLibHandler = ref)} />
+        <CanvasDataHandler onRef={ref => (this.CanvasDataHandler = ref)} />
       </div>
 
     );
---
+++ src/components/CanvasDataHandler.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Axios from 'axios'
+
+export default class CanvasDataHandler extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            story: ''
+        }
+    }
+    componentDidMount() {
+        this.props.onRef(this)
+    }
+    componentWillUnmount() {
+        this.props.onRef(undefined)
+    }
+
+    fetchRandomCourse = () => {
+        return "sdjkfnaskdjfn"
+    }
+    fetchRandomQuiz = () => {
+
+    }
+    fetchRandomAssignment = () => {
+
+    }
+    fetchRandomRoles = () => {
+
+    }
+    fetchRandomGroup = () => {
+
+    }
+    fetchRandomStudent = () => {
+
+    }
+    fetchRandomAdmin = () => {
+
+    }
+    fetchSelfName = () => {
+
+    }
+
+    render() {
+        return (<i></i>)
+    }
+}
