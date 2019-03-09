1a316ad1fc890a6cfcf33f949380c98310c70ad9
Joe V <joevillegasisawesome@gmail.com>
2019-03-09 00:08:16 -0800

    Added functionality for fetching users, modules,et

--- src/components/CanvasDataHandler.jsx
+++ src/components/CanvasDataHandler.jsx
@@ -20,7 +20,7 @@ export default class CanvasDataHandler extends React.Component {
                 access_token: '',
             },
 
-            data: {}
+            data: this.props.data
         }
     }
     componentDidMount = () => {
@@ -31,10 +31,10 @@ export default class CanvasDataHandler extends React.Component {
     }
 
     /*  Fetch Item Lists  */
-    formData = async () => {
+    initializeData = async () => {
         let courses = await this.fetchCourses()
-        let assignments = await this.fetchAssignments()
-        //let modules = await this.fetchModules()
+        let groups = await this.fetchGroups()
+
         //console.log('data formed')
 
         return { courses }
@@ -75,7 +75,7 @@ export default class CanvasDataHandler extends React.Component {
         )
             .then(res => {
                 //console.log(course_id)
-                console.log(`Assignments found in ${course_id}: ${res.data.length}`)
+                //console.log(`Assignments found in ${course_id}: ${res.data.length}`)
                 if (res.data.length === 0) return null
 
 
@@ -122,8 +122,8 @@ export default class CanvasDataHandler extends React.Component {
             this.props.headers
         )
             .then(res => {
-                console.log(res.data)
-                console.log(`Modules found in ${course_id}: ${res.data.length}`)
+                //console.log(res.data)
+                //console.log(`Modules found in ${course_id}: ${res.data.length}`)
 
 
                 return res.data
@@ -135,46 +135,32 @@ export default class CanvasDataHandler extends React.Component {
 
         return data
     }
-    fetchStudents = async (course_id = this.fetchRandomCourseID(),
+    fetchUsers = async (course_id = this.fetchRandomCourseID(),
         token = credentials.canvas.access_token,
         install_url = credentials.canvas.install_url) => {
 
         var token_string = `?access_token=${token}`
-        var type_string = `/api/v1/courses/${await course_id}/users?=enrollment_type[student]`
+        var type_string = `/api/v1/courses/${await course_id}/users`
 
-        await Axios.get(`${install_url + type_string + token_string}`, {
-            crossDomain: true
-        })
+        const data = await Axios.get(`${this.props.proxy_url +
+            install_url + type_string + token_string}`,
+            this.props.headers
+        )
             .then(res => {
                 console.log(res.data)
-                this.state.data.students.push({
+                console.log(`Users found in ${course_id}: ${res.data.length} `)
+
+                return res.data
+                this.state.data.users.push({
                     course_id: `${course_id}`,
-                    course_students: res
+                    course_users: res
                 })
             })
 
-        return this.state.data.students
+        return data
     }
-    fetchTeachers = async (course_id = this.fetchRandomCourseID(),
-        token = credentials.canvas.access_token,
-        install_url = credentials.canvas.install_url) => {
 
-        var token_string = `?access_token=${token}`
-        var type_string = `/api/v1/courses/${await course_id}/users?=enrollment_type[teacher]`
-
-        await Axios.get(`${install_url + type_string + token_string}`, {
-            crossDomain: true
-        })
-            .then(res => {
-                console.log(res.data)
-                this.state.data.teachers.push({
-                    course_id: `${course_id}`,
-                    course_teachers: res
-                })
-            })
-
-        return this.state.data.teachers
-    }
+    //  Normally returns null unless Admin
     fetchAccounts = async (
         token = credentials.canvas.access_token,
         install_url = credentials.canvas.install_url) => {
@@ -193,32 +179,13 @@ export default class CanvasDataHandler extends React.Component {
                     this.state.data.accounts = res
                 }).catch(err => {
                     console.error(err)
-                    console.log('Canvas account not found.')
+                    console.log('Canvas accounts not found.')
                     return false
                 })
         if (!account_exists) return null;
 
 
         return this.state.data.accounts
-    }
-    //TODO: Functionalize with account's 'users'
-    fetchRoles = async (account_id = this.state.data.accounts[0].id,
-        token = credentials.canvas.access_token,
-        install_url = credentials.canvas.install_url) => {
-
-        var token_string = `?access_token=${token}`
-        var type_string = `/api/v1/accounts/${account_id}/roles`
-
-        await Axios.get(`${this.props.proxy_url +
-            install_url + type_string + token_string}`, {
-                crossDomain: true
-            })
-            .then(res => {
-                console.log(res.data)
-                this.state.data.roles = res
-            })
-
-        return this.state.data.roles
     }
     fetchGroups = async (
         token = credentials.canvas.access_token,
@@ -227,16 +194,98 @@ export default class CanvasDataHandler extends React.Component {
         var token_string = `?access_token=${token}`
         var type_string = `/api/v1/users/self/groups`
 
+        const data = await Axios.get(`${this.props.proxy_url +
+            install_url + type_string + token_string}`,
+            this.props.headers
+        )
+            .then(res => {
+                this.state.data.groups = res.data
+
+                console.log('Groups found in user: ')
+                console.log(this.state.data.groups)
+                return res.data
+            })
+
+
+        return data
+    }
+
+    /*
+    THESE METHODS REQUIRE A USER ID.
+    TODO: Plug in account/user id
+    */
+    fetchStudents = async (course_id = this.fetchRandomCourseID(),
+        token = credentials.canvas.access_token,
+        install_url = credentials.canvas.install_url) => {
+
+        var token_string = `?access_token=${token}`
+        var type_string = `/api/v1/courses/${await course_id}/users=enrollment_type[student]`
+
+        const data = await Axios.get(`${this.props.proxy_url +
+            install_url + type_string + token_string}`,
+            this.props.headers
+        )
+            .then(res => {
+                console.log(res.data)
+                console.log(`Students found in ${course_id}: ${res.data.length} `)
+
+                return res.data
+                this.state.data.students.push({
+                    course_id: `${course_id}`,
+                    course_students: res
+                })
+            })
+
+        return data
+    }
+
+    fetchTeachers = async (course_id = this.fetchRandomCourseID(),
+        token = credentials.canvas.access_token,
+        install_url = credentials.canvas.install_url) => {
+
+        var token_string = `?access_token=${token}`
+        var type_string = `/api/v1/courses/${await course_id}/users?=enrollment_type[teacher]`
+
         await Axios.get(`${install_url + type_string + token_string}`, {
             crossDomain: true
         })
             .then(res => {
                 console.log(res.data)
-                this.state.data.groups = res
+
+                return res.data
+                this.state.data.teachers.push({
+                    course_id: `${course_id}`,
+                    course_teachers: res
+                })
             })
 
-        return this.state.data.groups
+        return this.state.data.teachers
     }
+
+    fetchRoles = async (account_id = this.state.data.accounts[0].id,
+        token = credentials.canvas.access_token,
+        install_url = credentials.canvas.install_url) => {
+
+        var token_string = `?access_token=${token}`
+        var type_string = `/api/v1/accounts/${account_id}/roles`
+
+        const data = await Axios.get(`${this.props.proxy_url +
+            install_url + type_string + token_string}`,
+            this.props.headers
+        )
+            .then(res => {
+                this.state.data.roles = res.data
+
+                console.log('Roles found in user: ')
+                console.log(this.state.data.roles)
+                return res.data
+            })
+
+
+        return data
+    }
+
+    //  Self User ID required
     fetchSelf = async (
         token = credentials.canvas.access_token,
         install_url = credentials.canvas.install_url) => {
@@ -255,7 +304,15 @@ export default class CanvasDataHandler extends React.Component {
         return this.state.data.self
     }
 
-    /*  Fetch Random items */
+    /*  
+    FETCH RANDOM ITEMS
+
+    Use these after setting the state to include pre-generated data.
+
+    You can also throw in an existing list as a parameter when applicable.
+
+    TODO: Error handling and null cases
+     */
     fetchRandomCourse = async (course_list = this.state.data.courses) => {
 
         let dice = Math.floor(Math.random() * course_list.length)
@@ -268,35 +325,16 @@ export default class CanvasDataHandler extends React.Component {
 
         //logInfo()
         return course_list[dice]
-        //TODO: NULL CASES 
     }
+
     fetchRandomCourseID = async (course_list = this.state.data.courses) => {
 
         let dice = Math.floor(Math.random() * course_list.length)
 
         //console.log(`Fetched course ID ${course_list[dice].id}`)
         return course_list[dice].id
-        //TODO: NULL CASES 
     }
-    fetchRandomQuiz = async () => {
-        let quiz_list = this.fetchQuizzes()
 
-        let dice
-        for (var i = 0; i < quiz_list.length; i++)
-            dice += Math.floor(Math.random() * quiz_list[i].length)
-
-        let comb_index = 0;
-        for (i = 0; i < quiz_list.length; i++)
-            for (var j = 0; j < quiz_list[i].course_quizzes.length; j++)
-                if (comb_index === dice) return quiz_list[i].course_quizzes[j]
-                else comb_index++
-
-        console.log('fetched random quiz')
-
-        return this.quiz_list[0].course_quizzes[0]
-
-        //TODO: NULL CASES
-    }
     fetchRandomAssignment = async () => {
         let assignment_list = null
 
@@ -313,14 +351,33 @@ export default class CanvasDataHandler extends React.Component {
 
         let dice = Math.floor(Math.random() * assignment_list.length)
 
-        console.log('fetched random assignment')
+        //logInfo()
         return assignment_list[dice]
     }
+
+    fetchRandomQuiz = async () => {
+        let quiz_list = this.fetchQuizzes()
+
+        let dice
+        for (var i = 0; i < quiz_list.length; i++)
+            dice += Math.floor(Math.random() * quiz_list[i].length)
+
+        let comb_index = 0;
+        for (i = 0; i < quiz_list.length; i++)
+            for (var j = 0; j < quiz_list[i].course_quizzes.length; j++)
+                if (comb_index === dice) return quiz_list[i].course_quizzes[j]
+                else comb_index++
+
+        console.log('fetched random quiz')
+
+        return this.quiz_list[0].course_quizzes[0]
+    }
+
     fetchRandomModule = async () => {
         let module_list = null
 
         while (module_list == null)
-            module_list = await this.fetchAssignments()
+            module_list = await this.fetchModules()
 
 
         let logInfo = () => {
@@ -332,51 +389,102 @@ export default class CanvasDataHandler extends React.Component {
 
         let dice = Math.floor(Math.random() * module_list.length)
 
-        //logInfo()
+        logInfo()
         return module_list[dice]
     }
-    fetchRandomRole = () => {
-        let dice = Math.floor(Math.random(this.state.data.roles.length))
 
-        return this.state.data.roles[dice]
-        //TODO: NULL CASES 
+
+    fetchRandomGroup = async (group_list = this.state.data.groups) => {
+        let dice = Math.floor(Math.random() * group_list.length)
+
+        let logInfo = () => {
+            console.log(group_list)
+            console.log('fetched random group')
+            console.log(group_list[dice])
+        }
+
+        //logInfo()
+        return group_list[dice]
     }
-    fetchRandomGroup = () => {
-        let dice = Math.floor(Math.random(this.state.data.groups.length))
 
-        return this.state.data.groups[dice]
-        //TODO: NULL CASES 
+    fetchRandomRole = async (role_list = this.state.data.roles) => {
+        let dice = Math.floor(Math.random * role_list.length)
+
+        let logInfo = () => {
+            console.log(role_list)
+            console.log('fetched random role')
+            console.log(role_list[dice])
+        }
+
+        //logInfo()
+        return role_list[dice]
     }
-    fetchRandomStudent = () => {
-        let dice
-        for (var i = 0; i < this.state.data.students.length; i++)
-            dice += Math.floor(Math.random(this.state.data.students[i].length))
 
-        let comb_index = 0;
-        for (i = 0; i < this.state.data.students.length; i++)
-            for (var j = 0; j < this.state.data.students[i].course_students.length; j++)
-                if (comb_index === dice) return this.state.data.students[i].course_students[j]
-                else comb_index++
+    fetchRandomUser = async () => {
+        let user_list = null
 
-        return this.state.data.students[0].course_students[0]
+        while (user_list == null)
+            user_list = await this.fetchUsers()
+
+
+        let logInfo = () => {
+            console.log(user_list)
+            console.log('fetched random user')
+            console.log(user_list[dice])
+        }
+
+
+        let dice = Math.floor(Math.random() * user_list.length)
+
+        //logInfo()
+        return user_list[dice]
     }
-    fetchRandomTeacher = () => {
-        let dice
-        for (var i = 0; i < this.state.data.teachers.length; i++)
-            dice += Math.floor(Math.random(this.state.data.teachers[i].length))
 
-        let comb_index = 0;
-        for (i = 0; i < this.state.data.teachers.length; i++)
-            for (var j = 0; j < this.state.data.teachers[i].course_teachers.length; j++)
-                if (comb_index === dice) return this.state.data.teachers[i].course_teachers[j]
-                else comb_index++
+    fetchRandomStudent = async () => {
+        let student_list = null
 
-        return this.state.data.teachers[0].course_teachers[0]
+        while (student_list == null)
+            student_list = await this.fetchStudents()
+
+
+        let logInfo = () => {
+            console.log(student_list)
+            console.log('fetched random student')
+            console.log(student_list[dice])
+        }
+
+
+        let dice = Math.floor(Math.random() * student_list.length)
+
+        //logInfo()
+        return student_list[dice]
+    }
+
+    fetchRandomTeacher = async () => {
+        let teacher_list = null
+
+        while (teacher_list == null)
+            teacher_list = await this.fetchStudents()
+
+
+        let logInfo = () => {
+            console.log(teacher_list)
+            console.log('fetched random student')
+            console.log(teacher_list[dice])
+        }
+
+
+        let dice = Math.floor(Math.random() * teacher_list.length)
+
+        //logInfo()
+        return teacher_list[dice]
     }
     fetchSelfName = () => {
         return this.fetchSelf().name
     }
 
+
+    //  And finally...
     render() {
         return (<i></i>)
     }
