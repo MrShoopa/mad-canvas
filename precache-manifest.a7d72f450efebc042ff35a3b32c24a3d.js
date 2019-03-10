ce6cd10ff83d23b262f299c1bcd0f68a87b5da4c
Joe V <joevillegasisawesome@gmail.com>
2019-03-09 19:12:39 -0800

    Updates

---
+++ precache-manifest.a7d72f450efebc042ff35a3b32c24a3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b050f5c47b6a2c84a8d",
+    "url": "/mad-canvas/static/js/runtime~main.8b050f5c.js"
+  },
+  {
+    "revision": "4cc7140adcb0d6ee19dd",
+    "url": "/mad-canvas/static/js/main.4cc7140a.chunk.js"
+  },
+  {
+    "revision": "9d02795d5c000ae5f5a6",
+    "url": "/mad-canvas/static/js/2.9d02795d.chunk.js"
+  },
+  {
+    "revision": "4cc7140adcb0d6ee19dd",
+    "url": "/mad-canvas/static/css/main.5226f093.chunk.css"
+  },
+  {
+    "revision": "b173678f6f4e90c066f13f6a2f88e97e",
+    "url": "/mad-canvas/index.html"
+  }
+];
